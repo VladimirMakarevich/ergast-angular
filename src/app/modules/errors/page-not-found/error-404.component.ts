d9f39a94140b6ac41d8d3777696536a99439e113
Vladimir Makarevich <makarevich.dev@outlook.com>
2020-12-05 17:57:23 +0300

    feat: add `page not found` module

---
+++ src/app/modules/errors/page-not-found/error-404.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app-error-404',
+    templateUrl: './error-404.component.html',
+    styleUrls: ['./error-404.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class Error404Component {
+}
